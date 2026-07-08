e4a01d3a0a194d1b5812a935947c186ccc6039ad
Tanishq <tamhankartanishq5@gmail.com>
2026-07-08 13:27:40 +0530

    Completed Day 1

--- DAY3/indexing.py
+++
@@ -1,9 +0,0 @@
-#STING INDEXING
-name = "Tanishq"
-print(name[0])
-print(name[3])
-print(name[6])
-
-#LENGTH OF STRING
-city = "Mumbai"
-print(len(city))
--- DAY5/return_values.py
+++
@@ -1,8 +0,0 @@
-#RETURN STATEMENT
-def add(a,b):
-    return a+b
-result = add(10,20)
-print(result)
-def square(num):
-    return(num*num)
-print(square(5))
